0c2e4f01061be66a1d80061ccc4ebd8c5f5e983c
MateuszKnapik <m.knapik6@gmail.com>
2019-06-02 23:46:33 +0200

    Added printing znodes' tree

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,7 +3,9 @@ import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 
 public class App {
@@ -29,15 +31,15 @@ public class App {
     }
 
     private int childrenNumber(String path) throws KeeperException, InterruptedException {
-        List<String> children = this.zooKeeper.getChildren(path, false);
+        List<String> childrenPaths = this.zooKeeper.getChildren(path, false);
 
         int amount = 0;
-        for (String child : children) {
-            String childrenPath = path + "/" + child;
+        for (String childPath : childrenPaths) {
+            String childrenPath = path + "/" + childPath;
             amount += childrenNumber(childrenPath);
         }
 
-        amount += children.size();
+        amount += childrenPaths.size();
 
         return amount;
     }
@@ -49,6 +51,23 @@ public class App {
 
     private void stopProgram() {
         process.destroy();
+    }
+
+    private void printZnodeTree(String path, int indent) throws KeeperException, InterruptedException {
+        List<String> childrenPaths = zooKeeper.getChildren(path, false);
+
+        for(int i = 0 ; i < indent ; i++) {
+            System.out.print(" ");
+        }
+        System.out.println(path);
+
+        for (String childPath : childrenPaths) {
+            printZnodeTree(path + "/" + childPath, ++indent);
+        }
+    }
+
+    public String getZnodeName() {
+        return znodeName;
     }
 
     private class ProgramWatcher implements Watcher {
@@ -109,7 +128,15 @@ public class App {
 
         app.start();
 
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String line;
         while (true) {
+            System.out.print("Enter command: ");
+            line = bufferedReader.readLine();
+
+            if ("tree".equals(line)) {
+                app.printZnodeTree(app.getZnodeName(), 0);
+            }
         }
     }
 
